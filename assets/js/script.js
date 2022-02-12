4cd3921e4dd089d8d1306f8e7bab4faa7686e6ba
Farhad Azadaliyev <farhadja@code.edu.az>
2022-02-12 22:44:48 +0400

    header added

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+    $(".btn-left").mouseover(function () {
+        $(".drop-content-left").removeClass("d-none")
+    })
+    $(".btn-left").mouseleave(function () {
+        $(".drop-content-left").addClass("d-none")
+    })
+    $(".btn-right").mouseover(function () {
+        $(".drop-content-right").removeClass("d-none")
+    })
+    $(".btn-right").mouseleave(function () {
+        $(".drop-content-right").addClass("d-none")
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
