db2d1f880a1d343780c559423727418df510aa7b
Farhad Azadaliyev <farhadja@code.edu.az>
2022-02-13 20:37:53 +0400

    header completed,slider added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,47 @@ $(function () {
     $(".btn-left").mouseover(function () {
         $(".drop-content-left").removeClass("d-none")
     })
-    $(".btn-left").mouseleave(function () {
+    $(".drop-content-left").mouseleave(function () {
         $(".drop-content-left").addClass("d-none")
     })
     $(".btn-right").mouseover(function () {
         $(".drop-content-right").removeClass("d-none")
     })
-    $(".btn-right").mouseleave(function () {
+    $(".drop-content-right").mouseleave(function () {
         $(".drop-content-right").addClass("d-none")
     })
+    $("#home-btn").mouseover(function () {
+        $(".home-nav").addClass("visible")
+
+    })
+    $(".home-nav").mouseleave(function () {
+        $(".home-nav").removeClass("visible")
+    })
+    $("#shop-btn").mouseover(function () {
+        $(".shop-nav").addClass("visible")
+
+    })
+    $(".shop-nav").mouseleave(function () {
+        $(".shop-nav").removeClass("visible")
+    })
+    const nextIcon = '<i class="fas fa-arrow-right"></i>';
+    const prevIcon = '<i class="fas fa-arrow-left"></i>';
+    $('.advert-top').owlCarousel({
+        
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        items:1,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:450,
+        animateIn:true,
+        animateOut:true,
+        navText:[prevIcon,nextIcon],
+        loop:true,
+         dots:false,
+         nav:true
+    });
+    
 
 
 
