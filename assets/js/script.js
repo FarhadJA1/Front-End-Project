dc79c40dbd49916a6163b2798406e3a767a01aed
Farhad Azadaliyev <farhadja@code.edu.az>
2022-02-14 13:42:31 +0400

    timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,18 @@ $(function () {
          dots:false,
          nav:true
     });
+    var clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+          stop: function() {
+            $('.message').html('The clock has stopped!')
+          }
+        }
+    });
+    clock.setTime(220880);
+    clock.setCountdown(true);
+    clock.start();
     
 
 
