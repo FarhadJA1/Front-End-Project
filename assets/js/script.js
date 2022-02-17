7c54179999ebcc72389f6153ff2e786d942c914e
Farhad Azadaliyev <farhadja@code.edu.az>
2022-02-17 18:30:35 +0400

    shop page started

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,7 +119,30 @@ $(function () {
             }
         }
     })
-
+    $(document).on("click",".product-categories-plus",function () {
+        $(this).parent().parent().children().last().toggleClass("d-none");
+        $(this).toggleClass("fas fa-plus")
+        $(this).toggleClass("fas fa-minus")
+        if($(".product-categories-checkbox").css("height")>"255px"){
+            $(".product-categories-checkbox").css("overflow-y","scroll")
+        }
+        else{
+           
+            $(".product-categories-checkbox").css("overflow-y","auto")
+        }
+    })
+   
+$('#multi6').mdbRange({
+    width: '25%',
+    
+    single: {
+      active: true,
+      multi: {
+        active: true,
+        rangeLength: 2
+      },
+    }
+  });
 
 
 
