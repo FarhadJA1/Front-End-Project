c41018788a93d1c84cf3dd2d9ceae890ffe48d59
Farhad Azadaliyev <farhadja@code.edu.az>
2022-02-17 18:32:20 +0400

    shop page started

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,17 +132,6 @@ $(function () {
         }
     })
    
-$('#multi6').mdbRange({
-    width: '25%',
-    
-    single: {
-      active: true,
-      multi: {
-        active: true,
-        rangeLength: 2
-      },
-    }
-  });
 
 
 
